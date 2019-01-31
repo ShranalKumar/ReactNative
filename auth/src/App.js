ee9d38e0f9e3369a294d240c1a6488bbf2f8235e
DATACOM-NZ\ShranalK <shranalk@datacom.co.nz>
2019-02-01 11:22:41 +1300

    added config for firebase

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,8 +1,20 @@
+import firebase from 'firebase';
 import React, {Component} from 'react';
 import {View, Text} from 'react-native';
 import {Header} from './components/common';
 
 class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: "AIzaSyCi9ULLEwYU2vEbv3VxYbhtdHuotsncsts",
+            authDomain: "authentication-2af0e.firebaseapp.com",
+            databaseURL: "https://authentication-2af0e.firebaseio.com",
+            projectId: "authentication-2af0e",
+            storageBucket: "authentication-2af0e.appspot.com",
+            messagingSenderId: "622087144088"
+          });
+    }
+
     render() {
         return (
             <View>
