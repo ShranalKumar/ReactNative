5f232756cb88232ce9c3b2a4c219557ca895b480
DATACOM-NZ\ShranalK <shranalk@datacom.co.nz>
2019-02-01 11:59:23 +1300

    added firebase module and initialization

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,25 +1,26 @@
-import firebase from 'firebase';
+import firebase from '@firebase/app';
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View} from 'react-native';
 import {Header} from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
     componentWillMount() {
         firebase.initializeApp({
-            apiKey: "AIzaSyCi9ULLEwYU2vEbv3VxYbhtdHuotsncsts",
-            authDomain: "authentication-2af0e.firebaseapp.com",
-            databaseURL: "https://authentication-2af0e.firebaseio.com",
-            projectId: "authentication-2af0e",
-            storageBucket: "authentication-2af0e.appspot.com",
-            messagingSenderId: "622087144088"
-          });
+            apiKey: 'AIzaSyCi9ULLEwYU2vEbv3VxYbhtdHuotsncsts',
+            authDomain: 'authentication-2af0e.firebaseapp.com',
+            databaseURL: 'https://authentication-2af0e.firebaseio.com',
+            projectId: 'authentication-2af0e',
+            storageBucket: 'authentication-2af0e.appspot.com',
+            messagingSenderId: '622087144088'
+        });
     }
 
     render() {
         return (
             <View>
                 <Header headerText="Authentication"/>
-                <Text>An App</Text>
+                <LoginForm />
             </View>
         );
     }
