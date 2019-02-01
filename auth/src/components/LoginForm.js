419fd70e19043802645d4d559ea10cf9841698d9
DATACOM-NZ\ShranalK <shranalk@datacom.co.nz>
2019-02-01 15:44:40 +1300

    configured login form to clear after login and proper error handling

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -9,15 +9,31 @@ class LoginForm extends Component {
 
     onButtonPress() {        
         const {email, password} = this.state;
+
         this.setState({error: '', loading: true});
         firebase.auth().signInWithEmailAndPassword(email, password)
+            .then(this.onLoginSuccess.bind(this))
             .catch(() => {
                 firebase.auth().createUserWithEmailAndPassword(email, password)
-                    .catch(() => {
-                        this.setState({error: 'Authentication Failed.'});
-
-                    });
+                    .then(this.onLoginSuccess.bind(this))
+                    .catch(this.onLoginFail.bind(this));
             });
+    }
+
+    onLoginSuccess() {
+        this.setState({
+            email: '',
+            password: '',
+            loading: false,
+            error: ''
+        });
+    }
+
+    onLoginFail() {
+        this.setState({
+            error: 'Authentication Failed',
+            loading: false
+        });
     }
 
     renderButton() {
@@ -72,6 +88,6 @@ const styles = {
         alignSelf: 'center',
         color: 'red'
     }
-}
+};
 
 export default LoginForm;
