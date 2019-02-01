186b92605e30b582c095b4e0c26af4ea4502ab4a
DATACOM-NZ\ShranalK <shranalk@datacom.co.nz>
2019-02-01 14:30:00 +1300

    added authentication error handling

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -1,14 +1,21 @@
 import firebase from '@firebase/app';
 import '@firebase/auth';
 import React, {Component} from 'react';
+import {Text} from 'react-native';
 import {Card, CardSection, Button, Input} from './common';
 
 class LoginForm extends Component {
-    state = {email: '', password: ''};
+    state = {email: '', password: '', error: ''};
 
     onButtonPress() {
         const {email, password} = this.state;
-        firebase.auth().signInWithEmailAndPassword(email, password);
+        firebase.auth().signInWithEmailAndPassword(email, password)
+            .catch(() => {
+                firebase.auth().createUserWithEmailAndPassword(email, password)
+                    .catch(() => {
+                        this.setState({error: 'Authentication Failed.'});
+                    });
+            });
     }
 
     render() {
@@ -33,11 +40,23 @@ class LoginForm extends Component {
                     />
                 </CardSection>
 
+                <Text style={styles.errorTextStyle}>
+                    {this.state.error}
+                </Text>
+
                 <CardSection>
                     <Button onPress={this.onButtonPress.bind(this)}>Log In</Button>
                 </CardSection>
             </Card>
         );
+    }
+}
+
+const styles = {
+    errorTextStyle : {
+        fontSize: 20,
+        alignSelf: 'center',
+        color: 'red'
     }
 }
 
