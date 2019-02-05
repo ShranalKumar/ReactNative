6c261bde1ac30dc089a3ded83615a716d06ff504
DATACOM-NZ\ShranalK <shranalk@datacom.co.nz>
2019-02-05 15:36:48 +1300

    show auth failed

--- manager/src/components/LoginForm.js
+++ manager/src/components/LoginForm.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {View, Text} from 'react-native';
 import {connect} from 'react-redux';
 import {Card, CardSection, Input, Button} from './common';
 import {emailChanged, passwordChanged, loginUser} from '../actions';
@@ -15,6 +16,18 @@ class LoginForm extends Component {
     onButtonPress() {
         const {email, password} = this.props;
         this.props.loginUser({email, password});
+    }
+
+    renderError() {
+        if (this.props.error) {
+            return (
+                <View style={{backgroundColor: 'white'}}>
+                    <Text style={styles.errorTextStyle}>
+                        {this.props.error}
+                    </Text>
+                </View>
+            )
+        }
     }
 
     render() {
@@ -39,7 +52,9 @@ class LoginForm extends Component {
                     />
                 </CardSection>
 
-                <CardSection>
+                {this.renderError()}
+
+                <CardSection>                    
                     <Button onPress={this.onButtonPress.bind(this)}>
                         Log In
                     </Button>
@@ -52,8 +67,17 @@ class LoginForm extends Component {
 const mapStateToProps = state => {
     return {
         email: state.auth.email,
-        password: state.auth.password
+        password: state.auth.password,
+        error: state.auth.error
     };
 };
+
+const styles = {
+    errorTextStyle: {
+        fontSize: 20,
+        alignSelf: 'center',
+        color: 'red'
+    }
+}
 
 export default connect(mapStateToProps, {emailChanged, passwordChanged, loginUser})(LoginForm);
