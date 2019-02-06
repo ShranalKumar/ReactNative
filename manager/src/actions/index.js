08212c02e5dc79c86a75ac68ff3e6952fc1c5941
DATACOM-NZ\ShranalK <shranalk@datacom.co.nz>
2019-02-07 10:55:31 +1300

    able to navigate to another scene

--- manager/src/actions/index.js
+++ manager/src/actions/index.js
@@ -1,5 +1,6 @@
 import {EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS, LOGIN_USER_FAIL, LOGIN_USER} from './types';
 import * as firebase from 'firebase';
+import {Actions} from 'react-native-router-flux';
 
 export const emailChanged = (text) => {
     return {
@@ -18,7 +19,7 @@ export const passwordChanged = (text) => {
 export const loginUser = ({email, password}) => {
     return (dispatch) => {
         dispatch({ type: LOGIN_USER});
-        
+
         firebase.auth().signInWithEmailAndPassword(email, password)
             .then(user => loginUserSuccess(dispatch, user))
             .catch(() => {
@@ -40,4 +41,6 @@ const loginUserSuccess = (dispatch, user) => {
         type: LOGIN_USER_SUCCESS,
         payload: user
     });
+
+    Actions.employeeList();
 };
