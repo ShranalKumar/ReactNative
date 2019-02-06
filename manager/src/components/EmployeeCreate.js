387a82073c259220c101ed4fe398f27a03e5a37f
DATACOM-NZ\ShranalK <shranalk@datacom.co.nz>
2019-02-07 11:56:01 +1300

    setup employee create form to handle input

--- manager/src/components/EmployeeCreate.js
+++ manager/src/components/EmployeeCreate.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
+import {connect} from 'react-redux';
+import {employeeUpdate} from '../actions';
 import {Card, CardSection, Input, Button} from './common';
-
 
 class EmployeeCreate extends Component {
     render() {
@@ -9,7 +10,9 @@ class EmployeeCreate extends Component {
                 <CardSection>
                     <Input 
                         label='Name'
-                        plceholder='Jane'
+                        placeholder='Jane'
+                        value={this.props.name}
+                        onChangeText={value => this.props.employeeUpdate({prop: 'name', value})}
                     />
                 </CardSection>
 
@@ -17,6 +20,8 @@ class EmployeeCreate extends Component {
                     <Input 
                         label='Phone'
                         placeholder='123-456-7890'
+                        value={this.props.phone}
+                        onChangeText={value => this.props.employeeUpdate({prop: 'phone', value})}
                     />
                 </CardSection>
 
@@ -34,4 +39,9 @@ class EmployeeCreate extends Component {
     }
 }
 
-export default EmployeeCreate;
+const mapStateToProps = (state) => {
+    const {name, phone, shift} = state.employeeForm;
+    return {name, phone, shift};
+};
+
+export default connect(mapStateToProps, {employeeUpdate})(EmployeeCreate);
