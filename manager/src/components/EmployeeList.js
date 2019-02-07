2794d11ff8b442422887d78170e1644a8de8a011
DATACOM-NZ\ShranalK <shranalk@datacom.co.nz>
2019-02-07 15:50:13 +1300

    building data source using data from firebase

--- manager/src/components/EmployeeList.js
+++ manager/src/components/EmployeeList.js
@@ -1,11 +1,27 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, ListView} from 'react-native';
 import {connect} from 'react-redux';
 import {employeesFetch} from '../actions';
 
 class EmployeeList extends Component {
     componentWillMount() {
         this.props.employeesFetch();
+        this.createDataSource(this.props);       
+    }
+
+    componentWillReceiveProps(nextProps) {
+        // nextProps are the next set of prop that this component
+        // will be rendered with
+        // this.props is still the old set of props
+        this.createDataSource(this.nextProps);
+    }
+
+    createDataSource({employees}) {
+        const ds = new ListView.DataSource({
+            rowhasChanged: (r1, r2) => r1 !== r2
+        });
+
+        this.dataource = ds.cloneWithRows(employees);
     }
 
     render() {
