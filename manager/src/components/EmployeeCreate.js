95d321b850aee561859e6f1890fdb66af6058b00
DATACOM-NZ\ShranalK <shranalk@datacom.co.nz>
2019-02-07 13:41:33 +1300

    implemented picker for create employee form

--- manager/src/components/EmployeeCreate.js
+++ manager/src/components/EmployeeCreate.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import {Picker} from 'react-native';
 import {connect} from 'react-redux';
 import {employeeUpdate} from '../actions';
 import {Card, CardSection, Input, Button} from './common';
@@ -21,12 +22,24 @@ class EmployeeCreate extends Component {
                         label='Phone'
                         placeholder='123-456-7890'
                         value={this.props.phone}
-                        onChangeText={value => this.props.employeeUpdate({prop: 'phone', value})}
+                        onChangeText={value => this.props.employeeUpdate({prop: 'phone', value})}                        
                     />
                 </CardSection>
 
                 <CardSection>
-
+                    <Picker
+                        selectedValue={this.props.shift}
+                        onValueChange={value => this.props.employeeUpdate({prop: 'shift', value})}
+                        style={{flex: 1}}
+                    >
+                        <Picker.Item label='Monday' value='Monday' />
+                        <Picker.Item label='Tuesday' value='Tuesday' />
+                        <Picker.Item label='Wednesday' value='Wednesday' />
+                        <Picker.Item label='Thursday' value='Thursday' />
+                        <Picker.Item label='Friday' value='Friday' />
+                        <Picker.Item label='Saturday' value='Saturday' />
+                        <Picker.Item label='Sunday' value='Sunday' />
+                    </Picker>
                 </CardSection>
 
                 <CardSection>
