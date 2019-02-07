8cdb4897639c21c32a1667d7b6f8d3018facba04
DATACOM-NZ\ShranalK <shranalk@datacom.co.nz>
2019-02-07 16:09:14 +1300

    map employees object to array of objects for data source

--- manager/src/components/EmployeeList.js
+++ manager/src/components/EmployeeList.js
@@ -2,29 +2,31 @@ import React, {Component} from 'react';
 import {View, Text, ListView} from 'react-native';
 import {connect} from 'react-redux';
 import {employeesFetch} from '../actions';
+import _ from 'lodash';
 
 class EmployeeList extends Component {
     componentWillMount() {
         this.props.employeesFetch();
-        this.createDataSource(this.props);       
+        this.createDataSource(this.props);
     }
 
     componentWillReceiveProps(nextProps) {
         // nextProps are the next set of prop that this component
         // will be rendered with
         // this.props is still the old set of props
-        this.createDataSource(this.nextProps);
+        this.createDataSource(nextProps);
     }
 
     createDataSource({employees}) {
         const ds = new ListView.DataSource({
-            rowhasChanged: (r1, r2) => r1 !== r2
+            rowHasChanged: (r1, r2) => r1 !== r2
         });
 
         this.dataource = ds.cloneWithRows(employees);
     }
 
     render() {
+        console.log(this.props);
         return (
             <View>
                 <Text>EmployeeList</Text>
@@ -38,4 +40,12 @@ class EmployeeList extends Component {
     }
 }
 
-export default connect(null, {employeesFetch})(EmployeeList);
+const mapStateToProps = state => {
+    const employees = _.map(state.employees, (val, uid) => {
+        return {...val, uid};
+    });
+
+    return {employees};
+};
+
+export default connect(mapStateToProps, {employeesFetch})(EmployeeList);
